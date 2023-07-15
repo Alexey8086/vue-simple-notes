98e1429bd57592379d0c6b86d92e132537d77e07
Alexey8086 <Alexey8086@yandex.ru>
2023-07-15 18:07:39 +0500

    refactoring file structure

--- simple-notes-app/Client/src/globalFunctions/customLog.js
+++
@@ -1,7 +0,0 @@
-export default function customLog (param) {
-    // логика функции выполняется только в режиме разработки
-    if (process.env.VUE_APP_MODE == 'development') {
-        console.log('\x1b[38;2;164;232;87m%s\x1b[0m', 'Custom log message:')
-        console.log(param)
-    }
-}
--- simple-notes-app/routes/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const mainRouter = require('./mainRouter')
-const notesRouter = require('./notesRouter')
-
-router.use('/user', mainRouter)
-router.use('/notes', notesRouter)
-
-module.exports = router
